5fb538ff311d17ed104ae230f2506be454e52cca
Miho19 <miho19@hotmail.co.nz>
2024-04-30 21:41:12 +1200

    Added initial hooks for Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Login from "./components/Login/Login";
 import Home from "./components/MainContent/Home/Home";
 import Sidebar from "./components/Sidebar/Sidebar";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
+  const { user, isAuthenicated, isLoading } = useAuth0();
+
+  if (!isAuthenicated) return <Login />;
+
   return (
     <div className="w-full h-[calc(100vh)] bg-black flex">
-      <Login />
-      {/* <Sidebar />
+      <Sidebar />
       <main className="w-3/4 h-full grow">
         <Home />
-      </main> */}
+      </main>
     </div>
   );
 }
