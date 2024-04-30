4200f3a675d4a9524a51c91ede9d94a6e1ec96db
Miho19 <miho19@hotmail.co.nz>
2024-04-30 21:22:49 +1200

    Auth0 now accepts spotify login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
+import Login from "./components/Login/Login";
 import Home from "./components/MainContent/Home/Home";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="w-full h-[calc(100vh)] bg-black flex">
-      <Sidebar />
+      <Login />
+      {/* <Sidebar />
       <main className="w-3/4 h-full grow">
         <Home />
-      </main>
+      </main> */}
     </div>
   );
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function Login() {
-  return <div>Login</div>;
+  const { loginWithRedirect } = useAuth0();
+  console.log(import.meta.env.VITE_AUTH0DOMAIN);
+  return (
+    <button
+      className="text-slate-950 w-20 h-20 bg-slate-100"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
 }
