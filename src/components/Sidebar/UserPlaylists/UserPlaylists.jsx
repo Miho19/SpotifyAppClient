11b5c00b7ff2840be85bee622387f5a0a67347bd
Miho19 <miho19@hotmail.co.nz>
2024-04-30 20:34:56 +1200

    Added initial testing for rendering components, moved components into specific files

---
+++ src/components/Sidebar/UserPlaylists/UserPlaylists.jsx
@@ -0,0 +1,10 @@
+export default function UserPlaylists() {
+  return (
+    <div
+      className="backgroundCard h-16 my-2 text-white p-3"
+      data-testid="userPlaylist"
+    >
+      User playlist
+    </div>
+  );
+}
