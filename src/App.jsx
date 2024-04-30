e5acb6d5dc62fe4100642a19564dc4a9befbe083
Miho19 <miho19@hotmail.co.nz>
2024-05-01 01:02:08 +1200

    Added in a loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import LoadingPage from "./components/Loading/LoadingPage";
 import Login from "./components/Login/Login";
 import Home from "./components/MainContent/Home/Home";
 import Sidebar from "./components/Sidebar/Sidebar";
@@ -7,6 +8,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) return <LoadingPage title="Fetching User" />;
 
   if (!isAuthenticated) return <Login />;
 
