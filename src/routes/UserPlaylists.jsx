1366e1275e04f6e51bdaecb8f5ff2b770c702053
Miho19 <miho19@hotmail.co.nz>
2024-05-27 01:30:30 +1200

    Playlist header styling

--- src/routes/UserPlaylists.jsx
+++ src/routes/UserPlaylists.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import useSpotifyPlaylist from "../hooks/useSpotifyPlaylist";
 import PlaylistHeader from "../components/Playlist/PlaylistHeader";
-
+import Loader from "react-js-loader";
 export default function UserPlaylists() {
   const { playlistID } = useParams();
   const spotifyPlaylistQuery = useSpotifyPlaylist(playlistID);
 
-  console.log(spotifyPlaylistQuery.data);
+  if (spotifyPlaylistQuery.isLoading) {
+    return (
+      <section className="h-full w-full bg-[#0f0f0f]">
+        <Loader type="bubble-top" bgColor="white" size={10} />
+      </section>
+    );
+  }
 
   return (
     <section className="h-full w-full bg-[#0f0f0f]">
-      <PlaylistHeader playlistData={spotifyPlaylistQuery.data} />
+      <div className="h-1/6 w-full">
+        <PlaylistHeader playlistData={spotifyPlaylistQuery.data} />
+      </div>
+      <div className="h-5/6 w-full border border-white"></div>
     </section>
   );
 }
